c18f40d30795daf3546a890375b64cc6ca8d7d61
janusle <janusle@gmail.com>
2016-04-01 01:02:50 +1100

    add pipeline to dump result to the queue

--- crawlers/SpecialFinder/pipelines.py
+++ crawlers/SpecialFinder/pipelines.py
@@ -1,11 +1,38 @@
 # -*- coding: utf-8 -*-
+from kombu.pools import producers
+from kombu import Exchange, Queue, Connection
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+scrapy_exchange = Exchange('scrapy_exchange', type='fanout')
+scrapy_result_queues = [
+                        # For analysing the result
+                        Queue('scrapy_result_analysis', scrapy_exchange),
+                        # For dumping to the database
+                        Queue('scrapy_result_db', scrapy_exchange),
+                       ]
 
 
-class SpecialfinderPipeline(object):
+class QueueDumperPipeline(object):
+
+    def __init__(self, queue_url):
+        self.queue_url = queue_url
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            queue_url=crawler.settings.get('QUEUE_URL')
+        )
+
+    def open_spider(self, spider):
+        self.connection = Connection(self.queue_url)
+
     def process_item(self, item, spider):
+        item_dict = {}
+        for key, value in item.items():
+            item_dict[key] = value
+
+        with producers[self.connection].acquire(block=True) as producer:
+            producer.publish(item_dict,
+                              serializer='msgpack',
+                              exchange=scrapy_exchange,
+                              declare=[scrapy_exchange]+scrapy_result_queues)
         return item
--- crawlers/SpecialFinder/settings.py
+++ crawlers/SpecialFinder/settings.py
@@ -61,9 +61,9 @@ NEWSPIDER_MODULE = 'SpecialFinder.spiders'
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'SpecialFinder.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'SpecialFinder.pipelines.QueueDumperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
